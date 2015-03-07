18b30149061785f4cf9255e5917938fc83476c69
dgriego <daniel@jackrabbitsystems.com>
2015-03-06 18:16:41 -0700

    adds variable exercises

--- variables/name.rb
+++ variables/name.rb
@@ -1,3 +1,12 @@
 print "Please enter your name: "
 name = gets
 puts "Hello #{name}"
+
+puts "\nPrinting your name 10 times: "
+10.times { puts name }
+
+puts "\nPlease enter you first name again: "
+first_name = gets.chomp
+puts "now enter you last name: "
+last_name = gets.chomp
+puts "\n#{first_name} #{last_name}"
