ed9507ece7334798b6a5f1236739c87e445db91b
dgriego <daniel@jackrabbitsystems.com>
2015-03-25 14:56:38 -0600

    updating hashes file

--- flow_control/zero_to_100.rb
+++ flow_control/zero_to_100.rb
@@ -1,17 +1,17 @@
-# def check_number_range(num)
-#     case num
-#     when 0..50
-#         puts "your number is between 0 and 50"
-#     when 51..100
-#         puts "your number is between 51 than 100"
-#     else
-#         if num < 0
-#             puts "you've entered a negative number, only positive numbers allowed!"
-#         else
-#             puts "your number is greater than 100"
-#         end
-#     end
-# end
+def check_number_range(num)
+    case num
+    when 0..50
+        puts "your number is between 0 and 50"
+    when 51..100
+        puts "your number is between 51 than 100"
+    else
+        if num < 0
+            puts "you've entered a negative number, only positive numbers allowed!"
+        else
+            puts "your number is greater than 100"
+        end
+    end
+end
 
 # puts "Please enter a number between 0 and 100"
 # users_number = gets.chomp.to_i
